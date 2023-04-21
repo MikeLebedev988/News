a33a699a51cb5e2ea76d18449962aac9eba893fe
Лебедев Михаил Семенович <lebedev-ms@gse.globse.local>
2023-04-21 08:07:31 +0800

    added new and refactoring existing models

--- NewsPaper/news_sections/models.py
+++ NewsPaper/news_sections/models.py
@@ -7,3 +7,17 @@ class Section(models.Model):
     article_title = models.CharField(max_length=35)
     article_text = models.TextField()
     views = models.IntegerField()
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=60)
+    position = models.CharField(max_length=55)
+    employment_contract = models.IntegerField()
+    phone = models.CharField(max_length=13)
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=60)
+    price = models.FloatField(default=0.0)
+    composition = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
